941814e72864c173e7252c4c4621576b9fd00e4d
rn7s2 <mail@rn7s2.cn>
2023-04-08 22:16:34 +0800

    基本实现 KV-Pair 存取功能；TODO 优化合并、查找性能（归并排序、二分查找）

---
+++ src/TimeSeqX/NaiveKV/Main.java
@@ -0,0 +1,25 @@
+package TimeSeqX.NaiveKV;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        NaiveKV kv = new NaiveKV("1.kv");
+
+//        for (int i = 0; i < 1_0000; i++) {
+//            String randString = String.valueOf(Math.random());
+//            kv.put("key" + i, randString);
+//        }
+//
+//        System.out.println(kv.getSegmentCount());
+
+        kv.put("你好", "世界");
+        System.out.println(kv.get("你好"));
+
+//        for (int i = 0; i < 1_0000; i++) {
+//            System.out.println("i=" + i + " " + kv.get("key" + i));
+//        }
+
+        kv.close();
+    }
+}
