94731c3b8a95e4b33a0f9c58e34954079d912a54
rn7s2 <mail@rn7s2.cn>
2023-04-09 14:54:55 +0800

    添加启动时重整和测试代码。

--- src/TimeSeqX/NaiveKV/Main.java
+++ src/TimeSeqX/NaiveKV/Main.java
@@ -1,22 +1,40 @@
 package TimeSeqX.NaiveKV;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        NaiveKV kv = new NaiveKV("1.kv");
+        int size = 5_000;
+        String filename = "naive.kv";
+        File f = new File(filename);
+        boolean ignore = f.delete();
 
-//        for (int i = 0; i < 1_0000; i++) {
-//            kv.put("key" + i, "value" + i);
-//        }
+        System.out.println("打开数据库，如果是第一次运行，会自动创建数据库文件");
+        System.out.println("如果不是第一次运行，则会重整数据库分片");
+        long startTime = System.currentTimeMillis();
+        NaiveKV kv = new NaiveKV(filename);
+        long endTime = System.currentTimeMillis();
+        System.out.println("耗时：" + (endTime - startTime) + "ms");
 
-        System.out.println(kv.getSegmentCount());
+        if (kv.get("1") == null) {
+            System.out.print("第一次运行，插入" + size + "条数据，");
+            startTime = System.currentTimeMillis();
+            for (int i = 0; i < size; i++) {
+                kv.put("" + i, "i^2=" + i * i);
+            }
+            endTime = System.currentTimeMillis();
+            System.out.println("耗时：" + (endTime - startTime) + "ms");
+        }
 
-        System.out.println(kv.get("key9997"));
-
-//        for (int i = 0; i < 1_0000; i++) {
-//            System.out.println("i=" + i + " " + kv.get("key" + i));
-//        }
+        System.out.print("随机读取" + size + "条数据，");
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            kv.get("" + (int)Math.floor(Math.random() * 1_0000));
+            // System.out.println("i=" + i + " " + kv.get("" + i));
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("耗时：" + (endTime - startTime) + "ms");
 
         kv.close();
     }
--- src/TimeSeqX/NaiveKV/NaiveKV.java
+++ src/TimeSeqX/NaiveKV/NaiveKV.java
@@ -22,6 +22,7 @@ public final class NaiveKV implements Closeable {
     private static final String RECORD_SEPERATOR = "" + (char) 30; // 记录分隔符，用于分隔键值对
     private static final String TOMBSTONE = "" + (char) 26; // 墓碑，表示键值对已被删除
     private static final int SEGMENT_MAX_COUNT = 8; // 数据库的最大片段数量
+    private static final int SEGMENT_MAX_CAPACITY_START = 1024; // 数据库的最大片段容量
     private int segment_max_capacity = 1024; // 目前最大片段容量
 
     private final RandomAccessFile file; // 数据库文件
@@ -33,6 +34,7 @@ public final class NaiveKV implements Closeable {
         // 获取数据库目前的最大片段容量
         if (file.length() != 0) {
             segment_max_capacity = file.readInt(); // 最大片段容量
+            reorganize();
         } else {
             file.writeInt(segment_max_capacity); // 最大片段容量
         }
@@ -109,54 +111,7 @@ public final class NaiveKV implements Closeable {
         int segmentCount = getSegmentCount();
         if (segmentCount >= SEGMENT_MAX_COUNT) { // 合并片段
             segment_max_capacity *= 2;
-            ArrayList<TreeMap<String, String>> newSegments = new ArrayList<>();
-            newSegments.add(new TreeMap<>(segment));
-            int newSegmentCount = 0;
-
-            // 从最后一个片段开始，合并片段（重新划分片段）
-            file.seek(file.length() - Long.BYTES); // 定位到最后一个片段的大小部分之前
-            while (true) {
-                // 读取本片段的内容
-                RawSegment segment = readSegment();
-
-                // 合并片段中的所有键值对
-                for (String segmentPart : segment.parts) {
-                    String[] kv = segmentPart.split(KV_SEPERATOR);
-                    if (kv[1].contains(TOMBSTONE)) {
-                        continue;
-                    }
-                    boolean contains = newSegments.stream().reduce(
-                            false,
-                            (a, b) -> a || b.containsKey(kv[0]),
-                            (a, b) -> a || b
-                    );
-                    if (contains) {
-                        continue;
-                    }
-                    if (newSegments.size() <= newSegmentCount) {
-                        newSegments.add(new TreeMap<>());
-                    }
-                    newSegments.get(newSegmentCount).put(kv[0], kv[1]);
-                    if (newSegments.get(newSegmentCount).size() >= segment_max_capacity) {
-                        newSegmentCount++;
-                    }
-                }
-
-                // 定位到上一个片段的大小部分之前
-                long lastSegmentOffset = file.getFilePointer() - segment.size;
-                if (lastSegmentOffset <= Integer.BYTES) {
-                    break;
-                }
-                file.seek(lastSegmentOffset);
-            }
-
-            // 将新的片段写入文件
-            file.setLength(0);
-            file.writeInt(segment_max_capacity); // 写入最大片段容量
-            Collections.reverse(newSegments); // 反转保证后写入最新的片段（最新片段在文件尾部）
-            for (TreeMap<String, String> newSegment : newSegments) {
-                writeSegment(newSegment);
-            }
+            mergeSegments();
         }
     }
 
@@ -164,6 +119,7 @@ public final class NaiveKV implements Closeable {
         put(key, TOMBSTONE);
     }
 
+    // 将当前更改同步到磁盘
     public void flush() throws IOException {
         if (segment.size() != 0) {
             file.seek(file.length()); // 定位到文件末尾
@@ -174,6 +130,48 @@ public final class NaiveKV implements Closeable {
         }
     }
 
+    // 计算合适的分片大小，并重新划分片段
+    public void reorganize() throws IOException {
+        int pairCount = getPairCount();
+        segment_max_capacity = Math.max((int) Math.ceil(pairCount / (SEGMENT_MAX_COUNT / 2.0)), SEGMENT_MAX_CAPACITY_START);
+        mergeSegments();
+    }
+
+    // 获取数据库中的键值对数量
+    public int getPairCount() throws IOException {
+        if (file.length() <= Integer.BYTES) { // 文件中没有任何片段
+            return 0;
+        }
+
+        TreeMap<String, String> allPairs = new TreeMap<>(segment);
+
+        // 从最后一个片段开始，合并片段（重新划分片段）
+        file.seek(file.length() - Long.BYTES); // 定位到最后一个片段的大小部分之前
+        while (true) {
+            // 读取本片段的内容
+            RawSegment segment = readSegment();
+
+            // 合并片段中的所有键值对
+            for (String segmentPart : segment.parts) {
+                String[] kv = segmentPart.split(KV_SEPERATOR);
+                if (kv[1].contains(TOMBSTONE)) {
+                    continue;
+                }
+                allPairs.putIfAbsent(kv[0], kv[1]);
+            }
+
+            // 定位到上一个片段的大小部分之前
+            long lastSegmentOffset = file.getFilePointer() - segment.size;
+            if (lastSegmentOffset <= Integer.BYTES) {
+                break;
+            }
+            file.seek(lastSegmentOffset);
+        }
+
+        return allPairs.size();
+    }
+
+    // 获取数据库中的片段数量
     public int getSegmentCount() throws IOException {
         if (file.length() <= Integer.BYTES) { // 文件中没有任何片段
             return segment.size() == 0 ? 0 : 1;
@@ -194,6 +192,7 @@ public final class NaiveKV implements Closeable {
         }
     }
 
+    // 片段信息
     private static final class RawSegment {
         public long size;
         public String[] parts;
@@ -204,6 +203,7 @@ public final class NaiveKV implements Closeable {
         }
     }
 
+    // 读取本片段的内容
     private RawSegment readSegment() throws IOException {
         long segmentSize = file.readLong(); // 本片段的大小
         long segmentOffset = file.getFilePointer() - segmentSize; // 本片段的偏移
@@ -217,6 +217,7 @@ public final class NaiveKV implements Closeable {
         return new RawSegment(segmentSize, segmentString.split(RECORD_SEPERATOR));
     }
 
+    // 将片段写入文件
     private void writeSegment(TreeMap<String, String> segment) throws IOException {
         // 将当前片段写入文件
         StringBuilder sb = new StringBuilder();
@@ -233,6 +234,63 @@ public final class NaiveKV implements Closeable {
         file.writeLong(segmentBytes.length + Long.BYTES); // 写入本片段的大小
     }
 
+    // 根据当前 segment_max_capacity 重新划分片段
+    private void mergeSegments() throws IOException {
+        if (file.length() <= Integer.BYTES && segment.size() == 0) { // 文件中没有任何片段
+            return;
+        }
+
+        ArrayList<TreeMap<String, String>> newSegments = new ArrayList<>();
+        newSegments.add(new TreeMap<>(segment));
+        int newSegmentCount = 0;
+
+        // 从最后一个片段开始，合并片段（重新划分片段）
+        file.seek(file.length() - Long.BYTES); // 定位到最后一个片段的大小部分之前
+        while (true) {
+            // 读取本片段的内容
+            RawSegment segment = readSegment();
+
+            // 合并片段中的所有键值对
+            for (String segmentPart : segment.parts) {
+                String[] kv = segmentPart.split(KV_SEPERATOR);
+                if (kv[1].contains(TOMBSTONE)) {
+                    continue;
+                }
+                boolean contains = newSegments.stream().reduce(
+                        false,
+                        (a, b) -> a || b.containsKey(kv[0]),
+                        (a, b) -> a || b
+                );
+                if (contains) {
+                    continue;
+                }
+                if (newSegments.size() <= newSegmentCount) {
+                    newSegments.add(new TreeMap<>());
+                }
+                newSegments.get(newSegmentCount).put(kv[0], kv[1]);
+                if (newSegments.get(newSegmentCount).size() >= segment_max_capacity) {
+                    newSegmentCount++;
+                }
+            }
+
+            // 定位到上一个片段的大小部分之前
+            long lastSegmentOffset = file.getFilePointer() - segment.size;
+            if (lastSegmentOffset <= Integer.BYTES) {
+                break;
+            }
+            file.seek(lastSegmentOffset);
+        }
+
+        // 将新的片段写入文件
+        file.setLength(0);
+        file.writeInt(segment_max_capacity); // 写入最大片段容量
+        Collections.reverse(newSegments); // 反转保证后写入最新的片段（最新片段在文件尾部）
+        for (TreeMap<String, String> newSegment : newSegments) {
+            writeSegment(newSegment);
+        }
+    }
+
+    // 刷写后关闭文件，释放资源
     @Override
     public void close() throws IOException {
         flush();
